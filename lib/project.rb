0b899a74ad27208f7381e0f1fe8e31430ae5b63e
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-14 20:13:27 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,9 @@
 class Project 
-  attr_reader :name 
+  attr_reader :title
   attr_accessor :backers
   
-  def initialize(name)
-    @name = name 
+  def initialize(title)
+    @title = title 
     @backers = []
   end 
   
